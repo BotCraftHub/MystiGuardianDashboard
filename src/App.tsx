e2246f26449074442c0498484db0e253a60ad0bf
Yusuf Arfan Ismail <yusufgamer222@gmail.com>
2023-01-29 17:15:02 +0000

    save

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
+import {Route, Routes} from "react-router";
+import {Home} from "./pages/Home";
 
 function App() {
-  return (
-    <div>
-      hello world
-    </div>
-  );
+  return <Routes>
+    <Route path="/" element={<Home />} />
+  </Routes>
 }
 
 export default App;
