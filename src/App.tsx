2d6243113fefa4a4632537bf7b8e0013738c71c8
Yusuf Arfan Ismail <yusufgamer222@gmail.com>
2023-01-29 20:28:01 +0000

    finished for today

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import {Route, Routes} from "react-router";
 import {Home} from "./pages/Home";
+import {Menu} from "./pages/Menu";
+import {Category} from "./pages/Category";
+import {SetWelcomeMessage} from "./pages/config/SetWelcomeMessgePage";
+import {SetLeaveMessage} from "./pages/config/SetLeaveMessagePage";
+import {SetLanguage} from "./pages/config/SetLanguagePage";
+import {EnableOrDisableWelcomeMessage} from "./pages/config/EnableOrDisbaleWelcomeMessagePage";
 
 function App() {
   return <Routes>
     <Route path="/" element={<Home />} />
+    <Route path="/menu" element={<Menu />} />
+    <Route path="/categories" element={<Category />} />
+    <Route path="/guild/set-welcome-message" element={<SetWelcomeMessage />} />
+    <Route path="/guild/set-leaving-message" element={<SetLeaveMessage />} />
+    <Route path="/guild/set-language" element={<SetLanguage />} />
+    <Route path="/guild/enable-or-disable-welcome-message" element={<EnableOrDisableWelcomeMessage />} />
   </Routes>
 }
 
