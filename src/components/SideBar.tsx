b9d54ce26f98aad91ee0cd96abcb46bb9adc7897
Yusuf Arfan Ismail <yusufgamer222@gmail.com>
2023-02-06 06:35:03 +0000

    begging work on sidebar

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,4 +1,23 @@
 //TODO: Add a sidebar component
-export const Sidebar = () => {
 
+import React from "react";
+
+export const Sidebar = () => {
+    return (
+        {/*} <Men>
+            <a className="menu-item" href="/">
+                Home
+            </a>
+            <a className="menu-item" href="/salads">
+                Salads
+            </a>
+            <a className="menu-item" href="/pizzas">
+                Pizzas
+            </a>
+            <a className="menu-item" href="/desserts">
+                Desserts
+            </a>
+        </Men> */}
+        
+    );
 }
